f704271620498a2e3d4a2c587b4b0857ff2ba30b
Zoia Krasheninnikova <krash76@mail.ru>
2020-07-22 18:44:07 +0200

    task.js changed

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,24 +1,39 @@
 "use strict";
 
-const getHole = (index) => {
- document.getElementById(`hole${index}`);
+//Вопросы, заданные в момент отправки, уже неактуальны. Разобралась )
+
+const deadCounter = document.getElementById("dead");
+const lostCounter = document.getElementById("lost");
+
+function getHole(index) {
+ return document.getElementById(`hole${index}`);
 }
- 
+
 for (let index = 1; index < 10; index++) {
-  getHole.onclick = function () {
-    if (getHole.classList.contains("hole_has-mole")) {
-      const deadCounter = document.getElementById("dead");
+  getHole(index).onclick = function () {
+    if (getHole(index).classList.contains("hole_has-mole")) {
       let count = Number(deadCounter.textContent);
       count += 1;
       deadCounter.textContent = String(count);
     } else {
-      const lostCounter = document.getElementById("lost");
       let count = Number(lostCounter.textContent);
       count += 1;
       lostCounter.textContent = String(count);
-    }  
+    }
+    let sumClicks = Number(deadCounter.textContent) + Number(lostCounter.textContent);
+    if (sumClicks === 10) { 
+      if (Number(deadCounter.textContent) > Number(lostCounter.textContent)) {
+        alert('Вы выиграли!');
+        deadCounter.textContent = 0;
+        lostCounter.textContent = 0;
+      } else {
+        alert('Вы проиграли');
+        deadCounter.textContent = 0;
+        lostCounter.textContent = 0;
+      }
+    }
   }
 }
- 
 
+ 
 
