96a18088fe848d54d922ebf16c718cb5994eca50
Zoia Krasheninnikova <krash76@mail.ru>
2020-07-22 16:01:04 +0200

    task.js changed

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,27 +1,24 @@
 "use strict";
 
 const getHole = (index) => {
-  document.getElementById(`hole${index}`);
-};
-
-//let hole = getHole();
-
-
-function controlClick() {
-   
-  if (hole.classList.contains("hole_has-mole")) {
-    const deadCounter = document.getElementById("dead");
-    let count = Number(deadCounter.textContent);
-    count += 1;
-    deadCounter.textContent = String(count);
-  } else {
-    const lostCounter = document.getElementById("lost");
-    let count = Number(lostCounter.textContent);
-    count += 1;
-    lostCounter.textContent = String(count);
-    }
+ document.getElementById(`hole${index}`);
 }
-
-hole.onclick = controlClick;
+ 
+for (let index = 1; index < 10; index++) {
+  getHole.onclick = function () {
+    if (getHole.classList.contains("hole_has-mole")) {
+      const deadCounter = document.getElementById("dead");
+      let count = Number(deadCounter.textContent);
+      count += 1;
+      deadCounter.textContent = String(count);
+    } else {
+      const lostCounter = document.getElementById("lost");
+      let count = Number(lostCounter.textContent);
+      count += 1;
+      lostCounter.textContent = String(count);
+    }  
+  }
+}
+ 
 
 
