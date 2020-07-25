0de925cfdd7354d53338655e404de81f7662a392
Zoia Krasheninnikova <krash76@mail.ru>
2020-07-25 22:21:05 +0200

    task.js changed

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,16 +1,9 @@
 "use strict";
 
-// 1) какой порядок кода предпочтительней?
-// 2) можно но ли еще оптимизировать?
-
-//вариант 1
-
 const mainModal = document.getElementById("modal_main");
+const successShow = document.getElementsByClassName("show-success");
 const successModal = document.getElementById("modal_success");
 const modalsToClose = document.getElementsByClassName("modal__close modal__close_times");
-const arrayToClose = Array.from(modalsToClose);
-const mainModalToClose = arrayToClose[0];
-const successModalToClose = arrayToClose[1];
 
 const activateMain = () => mainModal.className = "modal modal_active";
 const disactivateMain = () => mainModal.className = "modal";
@@ -19,38 +12,15 @@ const disactivateSuccess = () => successModal.className = "modal";
 
 activateMain();
 
-mainModal.onclick = function() {
+modalsToClose[0].onclick = function() {
+  disactivateMain();
+}
+
+successShow[0].onclick = function() {
   disactivateMain();
   activateSuccess();
 }
 
-successModalToClose.onclick = function () {
+modalsToClose[1].onclick = function () {
   disactivateSuccess();
 }
-
-//вариант 2
-
-// const mainModal = document.getElementById("modal_main");
-
-// const activateMain = () => mainModal.className = "modal modal_active";
-
-// activateMain();
-
-// const modalsToClose = document.getElementsByClassName("modal__close modal__close_times");
-// const arrayToClose = Array.from(modalsToClose);
-// const mainModalToClose = arrayToClose[0];
-// const disactivateMain = () => mainModal.className = "modal";
-// const activateSuccess = () => successModal.className = "modal modal_active";
-
-// mainModal.onclick = function() {
-//   disactivateMain();
-//   activateSuccess();
-// }
-
-// const successModal = document.getElementById("modal_success");
-// const successModalToClose = arrayToClose[1];
-// const disactivateSuccess = () => successModal.className = "modal";
-
-// successModalToClose.onclick = function () {
-//   disactivateSuccess();
-// }
