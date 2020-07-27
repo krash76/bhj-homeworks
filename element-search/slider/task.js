ff82fab5d8503aa6156056bc5c78c668cf9af2ad
Zoia Krasheninnikova <krash76@mail.ru>
2020-07-27 14:33:20 +0200

    task.js changed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,20 +2,19 @@
 
 const leftArrow = document.querySelector("div.slider__arrow.slider__arrow_prev");
 const rightArrow = document.querySelector("div.slider__arrow.slider__arrow_next");
-let sliders = document.querySelectorAll("div.slider__item");
-
 
 const activateSlide = (slide) => slide.classList.add("slider__item_active");
 const disactivateSlide = (slide) => slide.classList.remove("slider__item_active");
 
-// не могу сообразить как функцию написать(((
-
+function isActive(slide) {
+  return slide.className = "slider__item slider__item_active";
+}
+ 
 rightArrow.onclick = function() {
-  
-  for (let i = 0; i < sliders.length; i++) {
-    let y = i + 1;
-    disactivateSlide(sliders[i]);
-    activateSlide(sliders[y]); // почему здесь undefined?
-    }
-  }
-
+  let sliders = document.querySelectorAll("div.slider__item"); //почему не обновляется при повторном клике?
+  let slidersArray = Array.from(sliders);
+  let activeIndex = slidersArray.findIndex(isActive);
+  disactivateSlide(sliders[activeIndex]);
+  let nextIndex = activeIndex + 1;
+  activateSlide(sliders[nextIndex]); 
+}
