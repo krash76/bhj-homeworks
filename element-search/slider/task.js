fab18266fafdb6575e02cc258fe2b3da3da0b324
Zoia Krasheninnikova <krash76@mail.ru>
2020-07-27 15:13:35 +0200

    task.js changed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,19 +2,20 @@
 
 const leftArrow = document.querySelector("div.slider__arrow.slider__arrow_prev");
 const rightArrow = document.querySelector("div.slider__arrow.slider__arrow_next");
-
-const activateSlide = (slide) => slide.classList.add("slider__item_active");
-const disactivateSlide = (slide) => slide.classList.remove("slider__item_active");
+let sliders = document.querySelectorAll("div.slider__item");
 
 function isActive(slide) {
   return slide.className = "slider__item slider__item_active";
 }
  
 rightArrow.onclick = function() {
-  let sliders = document.querySelectorAll("div.slider__item"); //почему не обновляется при повторном клике?
   let slidersArray = Array.from(sliders);
   let activeIndex = slidersArray.findIndex(isActive);
-  disactivateSlide(sliders[activeIndex]);
+  sliders[activeIndex].classList.remove("slider__item_active");
+  slidersArray[activeIndex].className = "slider__item";
+
   let nextIndex = activeIndex + 1;
-  activateSlide(sliders[nextIndex]); 
-}
+  sliders[nextIndex].classList.add("slider__item_active");
+  slidersArray[nextIndex].className = "slider__item slider__item_active";
+  //console.log(slidersArray)
+ }
