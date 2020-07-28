61a4a17898a67b2edd5992aa7080e2c89154054a
Zoia Krasheninnikova <krash76@mail.ru>
2020-07-28 09:17:05 +0200

    task.js changed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -5,7 +5,7 @@ const rightArrow = document.querySelector("div.slider__arrow.slider__arrow_next"
 let sliders = document.querySelectorAll("div.slider__item");
 
 function isActive(slide) {
-  return slide.className = "slider__item slider__item_active";
+  return slide.className === "slider__item slider__item_active";
 }
  
 rightArrow.onclick = function() {
@@ -14,8 +14,30 @@ rightArrow.onclick = function() {
   sliders[activeIndex].classList.remove("slider__item_active");
   slidersArray[activeIndex].className = "slider__item";
 
-  let nextIndex = activeIndex + 1;
-  sliders[nextIndex].classList.add("slider__item_active");
+  if (activeIndex < 4) {
+    let nextIndex = activeIndex + 1;
+    sliders[nextIndex].classList.add("slider__item_active");
   slidersArray[nextIndex].className = "slider__item slider__item_active";
-  //console.log(slidersArray)
- }
+  } else {
+    let nextIndex = 0;
+    sliders[nextIndex].classList.add("slider__item_active");
+    slidersArray[nextIndex].className = "slider__item slider__item_active";
+  }
+}
+
+leftArrow.onclick = function() {
+  let slidersArray = Array.from(sliders);
+  let activeIndex = slidersArray.findIndex(isActive);
+  sliders[activeIndex].classList.remove("slider__item_active");
+  slidersArray[activeIndex].className = "slider__item";
+
+  if (activeIndex === 0) {
+    let nextIndex = 4;
+    sliders[nextIndex].classList.add("slider__item_active");
+  slidersArray[nextIndex].className = "slider__item slider__item_active";
+  } else {
+    let nextIndex = activeIndex - 1;
+    sliders[nextIndex].classList.add("slider__item_active");
+    slidersArray[nextIndex].className = "slider__item slider__item_active";
+  }
+}
