fcf3c7a343db47b17cdf53cd7a424af104332058
Zoia Krasheninnikova <krash76@mail.ru>
2020-07-29 20:07:16 +0200

    task.js changed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,39 +3,36 @@
 const leftArrow = document.querySelector("div.slider__arrow.slider__arrow_prev");
 const rightArrow = document.querySelector("div.slider__arrow.slider__arrow_next");
 let sliders = document.querySelectorAll("div.slider__item");
+let slidersArray = Array.from(sliders);
+
+let nextIndex;
 
 function isActive(slide) {
   return slide.className === "slider__item slider__item_active";
 }
  
+function activate(slide) {
+  slide.className = "slider__item slider__item_active";
+}
+
 rightArrow.onclick = function() {
-  let slidersArray = Array.from(sliders);
   let activeIndex = slidersArray.findIndex(isActive);
-  sliders[activeIndex].classList.remove("slider__item_active");
   slidersArray[activeIndex].className = "slider__item";
-  if (activeIndex < 4) {
-    let nextIndex = activeIndex + 1;
-    sliders[nextIndex].classList.add("slider__item_active");
-  slidersArray[nextIndex].className = "slider__item slider__item_active";
+  if (activeIndex < (slidersArray.length - 1)) {
+    nextIndex = activeIndex + 1;
   } else {
-    let nextIndex = 0;
-    sliders[nextIndex].classList.add("slider__item_active");
-    slidersArray[nextIndex].className = "slider__item slider__item_active";
+    nextIndex = 0;
   }
+  activate(slidersArray[nextIndex]);
 }
 
 leftArrow.onclick = function() {
-  let slidersArray = Array.from(sliders);
   let activeIndex = slidersArray.findIndex(isActive);
-  sliders[activeIndex].classList.remove("slider__item_active");
   slidersArray[activeIndex].className = "slider__item";
   if (activeIndex === 0) {
-    let nextIndex = 4;
-    sliders[nextIndex].classList.add("slider__item_active");
-  slidersArray[nextIndex].className = "slider__item slider__item_active";
+    nextIndex = slidersArray.length - 1;
   } else {
-    let nextIndex = activeIndex - 1;
-    sliders[nextIndex].classList.add("slider__item_active");
-    slidersArray[nextIndex].className = "slider__item slider__item_active";
+    nextIndex = activeIndex - 1;
   }
+  activate(slidersArray[nextIndex]);
 }
