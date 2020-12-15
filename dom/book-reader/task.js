3b33128a3fb1cf0cd4d8c23403fa4f7296f5aef2
Zoia Krasheninnikova <krash76@mail.ru>
2020-12-15 20:04:47 +0100

    task.js changed

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,14 +1,22 @@
 "use strict";
 
 const fontSizes = document.getElementsByClassName('font-size');
-const divBook = document.getElementsByClassName('book__control book__control_font-size')
+
+const isActive = (el) => el.classList.contains('font-size_active');
+const activateSize = (el) => el.classList.add('font-size_active');
+const disactivateSize = (el) => el.classList.remove('font-size_active');
+
+const book = document.getElementsByClassName('book');
+const bigBook = () => book[0].classList.add('book_fs-big');
+const smallBook = () => book[0].classList.add('book_fs-small');
+const disactivateBook = () => book[0].className = 'book_fs-small';
+
 
 for (let i = 0; i < fontSizes.length; i++) {
   let element = fontSizes[i];
-  divBook[0].addEventListener('click', function() {
-      element.classList.toggle('font-size_active')
-    }
-  )
+  element.addEventListener('click', () => {
+         console.log(element); //что не так??? Почему клик на element не работает?
+  });
 }
 
- //что не так??? 
+ 
