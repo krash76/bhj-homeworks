d10091d5e30315c60e299281155b564912305ed9
Zoia Krasheninnikova <krash76@mail.ru>
2021-05-21 19:55:03 +0200

    task.js changed

---
+++ html-forms/chat/task.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const divChat = document.getElementsByClassName("chat-widget");
+
+divChat[0].onclick = () => {
+  divChat[0].className = "chat-widget chat-widget_active";
+}
+
+//console.log(divChat)
+
